c368ba11483820443c0da22eaacf66439b8f67b0
hpham912 <ngochoang5111@gmail.com>
2020-02-20 14:12:46 -0500

    Hoang: add google signin

--- CUSTOS/app/src/main/java/com/example/custos/SplashActivity.java
+++ CUSTOS/app/src/main/java/com/example/custos/SplashActivity.java
@@ -5,35 +5,74 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class SplashActivity extends AppCompatActivity {
-
-
+    SignInButton signInButton;
+    GoogleSignInClient googleSignInClient;
+    int RC_SIGN_IN =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        Button loginButton = findViewById(R.id.login_button);
-        Button signupButton = findViewById(R.id.sign_up_button);
-
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent loginIntent = new Intent(view.getContext(), LoginActivity.class);
-                startActivity(loginIntent);
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
             }
         });
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+            googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
+    }
+    private void signIn(){
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signupIntent = new Intent(view.getContext(), SignupActivity.class);
-                startActivity(signupIntent);
-            }
-        });
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        //Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if(requestCode == RC_SIGN_IN){
+            //The task returned from this call is always completed no need to attach a listener
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
 
     }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+        try{
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Intent intent = new Intent(SplashActivity.this,MapsActivity.class);
+            startActivity(intent);
+        }catch (ApiException e){
+            //the ApiException status code indicates the detailed failure reason
+            //Please refer to the googlesigninstatuscodes class reference for more info
+        }
+    }
+
+//TODO: saved last signed in google account
+//    @Override
+//    protected void onStart(){
+//        super.onStart();
+//        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+//
+//    }
 }
