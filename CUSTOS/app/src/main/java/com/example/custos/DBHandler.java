7fbfdf84dc95c501624bf4644c7d405e6664c14c
Ryan Pham <ryan.pham1896@yahoo.com>
2020-02-21 16:15:29 -0500

    Ryan Pham DB - DB Handler

---
+++ CUSTOS/app/src/main/java/com/example/custos/DBHandler.java
@@ -0,0 +1,12 @@
+package com.example.custos;
+
+import org.json.JSONObject;
+
+public class DBHandler {
+
+    public JSONObject getUser() {
+        JSONObject user = new JSONObject();
+        return user;
+    }
+
+}
