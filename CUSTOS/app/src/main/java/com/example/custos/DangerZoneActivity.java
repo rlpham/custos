23a2777d5bba9278c325506763da4f8791b5a59c
Dale Reyes <dalereyes4@gmail.com>
2020-02-22 01:46:39 -0500

    Dale Reyes: Adding in activity to AndroidManifest

--- CUSTOS/app/src/main/java/com/example/custos/DangerZoneActivity.java
+++ CUSTOS/app/src/main/java/com/example/custos/DangerZoneActivity.java
@@ -1,6 +1,7 @@
 package com.example.custos;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,15 @@ public class DangerZoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.danger_zone);
 
+        /**
+         * Risk Level Spinner Code
+         */
         Spinner riskLevelSpinner = findViewById(R.id.risklevelspinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.criticallevelarray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        riskLevelSpinner.setAdapter(adapter);
+
+        //TODO: Add in item listener
 
 
 
