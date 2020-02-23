a7d6c2b321fa1295a312fac260ed33476aacc065
hpham912 <ngochoang5111@gmail.com>
2020-02-22 21:22:14 -0500

    Hoang: search view

--- CUSTOS/app/src/main/java/com/example/custos/MapsActivity.java
+++ CUSTOS/app/src/main/java/com/example/custos/MapsActivity.java
@@ -12,12 +12,17 @@ import androidx.fragment.app.FragmentTransaction;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -28,26 +33,41 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.libraries.places.api.Places;
+//import com.google.android.libraries.places.api.model.TypeFilter;
+//import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private SearchView searchView;
     private FragmentTransaction transaction;
     private FusedLocationProviderClient fusedLocationClient;
-    private final int ok=0;
-
+    private final int ok = 0;
     //Testcode below
 
     public void openFragment(Fragment fragment) {
-        transaction=getSupportFragmentManager().beginTransaction();
+        transaction = getSupportFragmentManager().beginTransaction();
 
         transaction.addToBackStack(null);
         transaction.replace(R.id.container, fragment);
         transaction.commit();
     }
-    public void closeFragment(){
-        if (getSupportFragmentManager().getBackStackEntryCount() > 0){
-        getSupportFragmentManager().popBackStackImmediate();}
+
+    public void closeFragment() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStackImmediate();
+        }
 
     }
 
@@ -59,7 +79,38 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-       // bottomNavigation = findViewById(R.id.bottom_navigation);
+        //Hoang testing for search bar
+        searchView = findViewById(R.id.search_view_mapActivity);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+                if(location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
+        //end
+        // bottomNavigation = findViewById(R.id.bottom_navigation);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -67,7 +118,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        //TestCode below
+        //Rahul TestCode below
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -86,17 +137,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         return true;
 
 
-                }return true;}
+                }
+                return true;
+            }
         });
-
+        //rahul end
 
 
 
     }
-
-
-
-
 
 
     /**
@@ -110,40 +159,39 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        if(checkPermissions()) {
+        if (checkPermissions()) {
             googleMap.setMyLocationEnabled(true);
 
-        mMap = googleMap;
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
+            mMap = googleMap;
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
 
-                        LatLng sydney = new LatLng(location.getLatitude(),location.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                      //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-                        moveToCurrentLocation(sydney);
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
+                            LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                            //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                            moveToCurrentLocation(sydney);
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
 
+                            }
                         }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("MapDemoActivity", "Error trying to get last GPS location");
-                e.printStackTrace();
-            }
-        });
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                    e.printStackTrace();
+                }
+            });
         }
 
         // Add a marker in Sydney and move the camera
 
     }
 
-    private void moveToCurrentLocation(LatLng currentLocation)
-    {
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
+    private void moveToCurrentLocation(LatLng currentLocation) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
         // Zoom in, animating the camera.
         mMap.animateCamera(CameraUpdateFactory.zoomIn());
         // Zoom out to zoom level 10, animating with a duration of 2 seconds.
@@ -158,14 +206,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             return true;
         } else {
             // Here, thisActivity is the current activity
-           requestPermissions();
+            requestPermissions();
             return false;
         }
 
 
-
     }
-    private void requestPermissions(){
+
+    private void requestPermissions() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
