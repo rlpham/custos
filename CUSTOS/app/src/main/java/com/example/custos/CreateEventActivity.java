7ea99927cd643c83dce2b9181c9c989f3bdd2863
Ryan Le Pham <ryan.pham1896@yahoo.com>
2020-02-24 17:56:46 -0500

    Ryan Pham - Invite Guests to Event

--- CUSTOS/app/src/main/java/com/example/custos/CreateEventActivity.java
+++ CUSTOS/app/src/main/java/com/example/custos/CreateEventActivity.java
@@ -2,10 +2,10 @@ package com.example.custos;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.model.LatLng;
@@ -45,5 +45,18 @@ public class CreateEventActivity extends AppCompatActivity {
             public void onError(@NonNull Status status) {
             }
         });
+    }
+
+    public void inviteGuests(View view) {
+        Intent invGuestIntent = new Intent(this, InviteGuestsActivity.class);
+        startActivityForResult(invGuestIntent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1) {
+            System.out.println("GOT HERE BABY");
+        }
     }
 }
---
+++ CUSTOS/app/src/main/java/com/example/custos/InviteGuestsActivity.java
@@ -0,0 +1,22 @@
+package com.example.custos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class InviteGuestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_event);
+
+
+//        Intent invGuestIntent = new Intent();
+//        invGuestIntent.putExtra("Guests", "guests");
+//        setResult(1, invGuestIntent);
+//        finish();
+    }
+
+}
