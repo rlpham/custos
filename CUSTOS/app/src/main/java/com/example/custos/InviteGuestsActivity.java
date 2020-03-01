53045ceb1d9fff3a3cf36055649a6d6a270c4bcd
Ryan Le Pham <ryan.pham1896@yahoo.com>
2020-03-01 13:42:26 -0500

    Ryan Pham - Invite

--- CUSTOS/app/src/main/java/com/example/custos/InviteGuestsActivity.java
+++ CUSTOS/app/src/main/java/com/example/custos/InviteGuestsActivity.java
@@ -4,22 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class InviteGuestsActivity extends AppCompatActivity {
 
-
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_event);
+        setContentView(R.layout.invite_guests);
         // TODO: Set up checkable text views in list, then pass them into intent and test
         // https://abhiandroid.com/ui/checkedtextview
 
-//        Intent invGuestIntent = new Intent();
-//        invGuestIntent.putExtra("Guests", "guests");
-//        setResult(1, invGuestIntent);
-//        finish();
+        listView =findViewById(R.id.list);
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Ryan");
+        names.add("This");
+        names.add("asdgsa");
+        names.add("bar");
+        names.add("foo");
+        names.add("baz");
+        names.add("abcdefg");
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.invite_guests, names);
+        listView.setAdapter(adapter);
     }
 
     public void doneAddingGuests() {
