1b09003f796b2a2b05bac9eb315c5caa9045408a
hpham912 <ngochoang5111@gmail.com>
2020-03-13 16:24:42 -0400

    homeloc

--- CUSTOS/app/src/main/java/com/example/custos/SetHomeLocationActivity.java
+++ CUSTOS/app/src/main/java/com/example/custos/SetHomeLocationActivity.java
@@ -1,11 +1,21 @@
 package com.example.custos;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.Places;
@@ -16,21 +26,32 @@ import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 
 public class SetHomeLocationActivity extends AppCompatActivity {
 
-
+    TextView address;
+    Geocoder geocoder;
+    List<Address> addresses = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_location);
-        String apiKey = "AIzaSyCjncU-Fe5pQKOc85zuGoR9XEs61joNajc";
-
+        address = findViewById(R.id.home_address);
+        final String apiKey = "AIzaSyCjncU-Fe5pQKOc85zuGoR9XEs61joNajc";
         if(!Places.isInitialized()){
             Places.initialize(getApplicationContext(),apiKey);
         }
+
+        geocoder = new Geocoder(this,Locale.getDefault());
 
 
 //        placesClient = Places.createClient(this);
@@ -43,11 +64,16 @@ public class SetHomeLocationActivity extends AppCompatActivity {
         autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
         //autocompleteFragment.setLocationRestriction(RectangularBounds.newInstance(new LatLng(40.263680,-76.890739), new LatLng(40.285519,-76.650589)));
 // Set up a PlaceSelectionListener to handle the response.
+
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place place) {
                 final LatLng latLng = place.getLatLng();
+
                 Log.i("tester", "Place: " + latLng.latitude+"\n" + latLng.longitude);
+
+                stringAddress(latLng);
+
             }
 
             @Override
@@ -55,6 +81,24 @@ public class SetHomeLocationActivity extends AppCompatActivity {
                 Log.i("test" ,"An error occurred: " + status);
             }
         });
+
+    }
+    private void stringAddress(LatLng latLng){
+        if(!addresses.isEmpty()){
+            try {
+                addresses = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+                if(addresses.size()>0){
+                    String myAddress = addresses.get(0).getAddressLine(0);
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    address.setText(myAddress + " " + city + " " + state);
+                    System.out.println("---------------------------------"+myAddress + city);
+                    Log.i("test","address: "+ myAddress + city + state);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
