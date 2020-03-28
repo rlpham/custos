2a24b5e1af7146adae53f6405144c8379bbe92ca
hpham912 <ngochoang5111@gmail.com>
2020-03-28 16:01:47 -0400

    hoang: util common

---
+++ CUSTOS/app/src/main/java/com/example/custos/utils/Common.java
@@ -0,0 +1,10 @@
+package com.example.custos.utils;
+
+import com.example.custos.User;
+
+public class Common {
+    public static final String USER_INFORMATION = "User Information";
+    public static User loggedUser;
+    public static final String USER_UID_SAVE_KEY = "SaveUid";
+    public static final String TOKENS = "Tokens";
+}
