5f974977bc9a7c86a240e98b28c8abcf8a1145c9
hpham912 <ngochoang5111@gmail.com>
2020-04-01 04:42:41 -0400

    hoang update splash

---
+++ CUSTOS/app/src/main/java/com/example/custos/SplashActivity2.java
@@ -0,0 +1,31 @@
+package com.example.custos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity2 extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),SplashActivity.class);
+                startActivity(intent);
+            }
+        },5000);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+    }
+}
