10b7e4f8a8e677b745a85ff09bf9bd34b1f05b43
hpham912 <ngochoang5111@gmail.com>
2020-04-10 17:02:13 -0400

    hoang update names

--- CUSTOS/app/src/main/java/com/example/custos/utils/Common.java
+++ CUSTOS/app/src/main/java/com/example/custos/utils/Common.java
@@ -3,9 +3,9 @@ package com.example.custos.utils;
 public class Common {
     public static final String USER_INFORMATION     = "User Information";
     public static final String USER_ADDRESS         = "User Address";
-    public static final String USER_HOME_LAT        = "User Home Latitude";
-    public static final String USER_HOME_LNG        = "User Home Longitude";
-    public static final String HOME_LOC             = "Home Location";
+    public static final String USER_HOME_LAT        = "userHomeLatitude";
+    public static final String USER_HOME_LNG        = "userHomeLongitude";
+    public static final String HOME_LOC             = "homeLocation";
     public static final String FRIENDS              = "Friends";
     public static final String FRIEND_EMAIL         = "friendEmail";
     public static final String FRIEND_NAME          = "friendName";
@@ -20,9 +20,9 @@ public class Common {
     public static final String USER_PIN             = "userPIN";
     public static final String USER_NAME            = "userName";
     public static final String USER_EMAIL           = "userEmail";
-    public static final String EMERGENCY_CONTACT    = "Emergency Contact";
-    public static final String EMERGENCY_NAME       = "Name";
-    public static final String EMERGENCY_PHONE      = "Phone number";
+    public static final String EMERGENCY_CONTACT    = "emergencyContact";
+    public static final String EMERGENCY_NAME       = "emergency_name";
+    public static final String EMERGENCY_PHONE      = "emergency_phone";
     public static final String FROM_NAME            = "FromName";
     public static final String TO_NAME              = "ToName";
     public static final String TO_UID               = "ToUid";
